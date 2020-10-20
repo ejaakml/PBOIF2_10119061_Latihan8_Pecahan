69e7420dda4fa36186d8f8aed20fff889d3b0437
ejaakml <eximindotong@yahoo.com>
2020-10-20 17:48:29 +0700

    s

---
+++ src/pboif2/pkg10119061/latihan8/pecahan/PBOIF210119061Latihan8Pecahan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan8.pecahan;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+                
+        
+    }
+    
+}
